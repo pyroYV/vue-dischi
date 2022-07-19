018b508290982804ea2aec9d955fac16f3689cd1
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-19 14:23:32 +0200

    Feat: Main Structure

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <ThumbList/>
+
+</template>
+
+<script>
+import ThumbList from './ThumbList.vue'
+export default {
+    components:{
+        ThumbList,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ThumbList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    thumbs
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
